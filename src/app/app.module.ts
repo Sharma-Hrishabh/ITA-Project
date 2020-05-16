8cdfb4b78692efcf4483da3e4fc33280ceb3a913
Sharma-Hrishabh <30hrs1999@gmail.com>
2020-05-16 16:12:03 +0530

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DataCollectionComponent } from './data-collection/data-collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DataCollectionComponent
   ],
   imports: [
     BrowserModule,
