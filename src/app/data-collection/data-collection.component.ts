bbdcd86ebf81cb6c72a232a29a650ee816c69e81
Sharma-Hrishabh <30hrs1999@gmail.com>
2020-05-16 18:20:28 +0530

    add two-way binding for forms

--- src/app/data-collection/data-collection.component.ts
+++ src/app/data-collection/data-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from '../patient';
 
 @Component({
   selector: 'app-data-collection',
@@ -7,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataCollectionComponent implements OnInit {
 
+  patient : Patient = {
+    id:0,
+    first_name:"",
+    last_name:"",
+    state:"",
+    city:"",
+    contactno:""
+  };
+
+  collectPatientDetails() {
+    console.log(this.patient.first_name);
+  }
+ 
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/patient.ts
@@ -0,0 +1,8 @@
+export interface Patient {
+    id: number;
+    first_name: string;
+    last_name: string;
+    state: string;
+    city: string;
+    contactno: string;
+  };
