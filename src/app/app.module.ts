4b2a80c06d71cd8ec3f7c6ef9cce333016acf3dc
Sharma-Hrishabh <30hrs1999@gmail.com>
2020-05-19 11:31:16 +0530

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DataCollectionComponent } from './data-collection/data-collection.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DataCollectionComponent
+    DataCollectionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
