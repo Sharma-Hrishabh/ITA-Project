3a107649f3ddb1bccf4dd18d3a4a9b5d8c8fd7df
Sharma-Hrishabh <30hrs1999@gmail.com>
2020-05-20 15:29:05 +0530

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { DataCollectionComponent } from './data-collection/data-collection.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +12,8 @@ import { DataCollectionComponent } from './data-collection/data-collection.compo
 })
 export class AppComponent {
   title = 'covid19app';
+  items: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('items').valueChanges();
+  }
 }
