6d2516d7c34a562bcd5e2627763cbaf817f33ecf
Sharma-Hrishabh <30hrs1999@gmail.com>
2020-05-21 12:14:09 +0530

    use matreial ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { DataCollectionComponent } from './data-collection/data-collection.component';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { MatSidenav } from '@angular/material/sidenav';
 
 
 @Component({
@@ -13,7 +14,7 @@ import { auth } from 'firebase/app';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'covid19app';
+  title = 'COVID-19-InfoHub';
   items: Observable<any[]>;
   constructor(firestore: AngularFirestore,public auth: AngularFireAuth) {
     this.items = firestore.collection('items').valueChanges();
@@ -24,4 +25,23 @@ export class AppComponent {
   logout() {
     this.auth.signOut();
   }
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule  } from  '@angular/material/toolbar';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatListModule } from  '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -25,6 +31,13 @@ import { environment } from '../environments/environment';
     AngularFirestoreModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
